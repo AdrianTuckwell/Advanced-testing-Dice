98d0df9bd29b8ba89b6d239f6ea504cb8f6fe9a1
Adrian Tuckwell <adrian.tuckwell@gmail.com>
2016-11-03 14:24:01 +0000

    initial revision

---
+++ Game.java
@@ -0,0 +1,21 @@
+public class Game {
+  Rollable dice;
+  private int turnCount;
+
+  public Game(Rollable dice) {
+    this.dice = dice;
+    this.turnCount = 0;
+  }
+
+  public boolean nextTurn(){
+    int result = dice.roll();
+    this.turnCount++;
+    return result > 2;
+  }
+
+  // getters --------------------
+  public int getTurnCount() {
+    return this.turnCount;
+  }
+
+}
